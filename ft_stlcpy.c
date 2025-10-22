4af309412e7b5c64655f8c0adf66ca0b24c6f7ef
fritz 42 <fritz.42@example.com>
2025-10-22 15:37:34 +0100

    strlcat cpy with strlen, begun  strchr

--- ft_stlcpy.c
+++ ft_stlcpy.c
@@ -6,7 +6,7 @@
 /*   By: fhollman <fhollman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 08:02:22 by fhollman          #+#    #+#             */
-/*   Updated: 2025/10/22 12:15:43 by fhollman         ###   ########.fr       */
+/*   Updated: 2025/10/22 13:50:54 by fhollman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,11 @@ size_t	strlcpy(char	*dest, const char	*src, size_t	size)
 	size_t	src_len;
 	size_t	i;
 
-	src_len = 0;
-
-	while (src[src_len])
-		++src_len;
+	src_len = ft_strlen(src);
 	if (size == 0)
 		return (src_len);
 	i = 0;
-	while (src[i])
+	while (src[i] && i < size - 1)
 	{
 		dest[i] = src[i];
 		i++;
--- ft_strchr.c
+++ ft_strchr.c
@@ -1,33 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fhollman <fhollman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/22 10:19:37 by fhollman          #+#    #+#             */
-/*   Updated: 2025/10/22 13:49:34 by fhollman         ###   ########.fr       */
+/*   Created: 2025/10/22 13:53:12 by fhollman          #+#    #+#             */
+/*   Updated: 2025/10/22 14:06:17 by fhollman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-size_t	strlcat(char	*dst, const char	*src, size_t	size)
+char	*strchr(const char *s, int c)
 {
-	size_t	dst_len;
-	size_t src_len;
-	size_t	i;
-	
-	dst_len = ft_strlen(dst);
-	src_len = ft_strlen(src);
-	if (dst_len >= size)
-		return ((size + src_len));
-	i = 0;
-	while (src[i] && (dst_len + i) < (size -1))
-	{
-		dst[dst_len + i] = src[i];
-		++i;
-	}
-	dst[dst_len + i] = '\0';
-	return (src_len + dst_len);
+	while (*s && (*s != c))
+		++s;
+	if (*s == '\0')
+	return (s);
 }
