0a65e157320b395849cc77f1fb9a235756d92f26
Friedrich Hollmann <fritz.hollmann@gmail.com>
2025-10-30 17:33:54 +0000

    strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -3,21 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strmapi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fhollman <fhollman@student.42.fr>          +#+  +:+       +#+        */
+/*   By: home <home@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 12:02:01 by fhollman          #+#    #+#             */
-/*   Updated: 2025/10/30 12:06:22 by fhollman         ###   ########.fr       */
+/*   Updated: 2025/10/30 17:11:56 by home             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include
+#include "libft.h"
 
 /*
 *applies a function f to each char in s.
-*f gets index of
+*@param[f] pointer to function
+*@param[s] og sting
+*	@param[int] index of og sting
+*	@param[char] actual char in string
+*@return pointer to mapped string
 */
 
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
+	int	len;
+	char	*return_s;
 	
+	if ((s == NULL) || (f == NULL))
+		return(NULL);
+	len = ft_strlen(s);
+	return_s = malloc(len + 1);
+	if (return_s == NULL)
+		return (NULL);
+	function(s, return_s, f);
+	return (return_s);
+}
+
+void function(char const	*s, char	*return_s, char (*f)(unsigned int, char))
+{
+	int	index;
+
+	index = 0;
+	while (*s)
+	{
+		*return_s = f(index, *s);
+		return_s++;
+		s++;
+		index++;
+	}
+	*return_s = '\0';
 }
