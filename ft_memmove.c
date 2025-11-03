e8d2e49008d93f3c2cdd742adefea156826f1b3f
fritz 42 <fritz.42@example.com>
2025-11-03 15:27:02 +0000

    ttle bugfixing

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: fhollman <fhollman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 12:44:03 by fhollman          #+#    #+#             */
-/*   Updated: 2025/10/24 14:50:23 by fhollman         ###   ########.fr       */
+/*   Updated: 2025/11/03 15:26:54 by fhollman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,10 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	d = dest;
 	s = src;
 	temp = malloc(n);
-	while(--n)
+	if (!temp)
+		return (NULL);
+	n_save = n;
+	while(n--)
 	{
 		*temp = *s;
 		++temp;
@@ -43,12 +46,57 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	}
 	temp -= n_save;
 	n = n_save;
-	while(--n)
+	while(n--)
 	{
 		*d = *temp;
 		++d;
 		++temp;
 	}
 	free(temp);
-	return (dest);
+	return (dest-n_save);
 }
+
+
+#include "stdio.h"
+
+
+
+
+
+/*
+typedef struct s_test_case
+{
+	char	*input;
+	size_t		expected;
+	char	*desciption;
+}	t_test_case;
+
+int	test_memmove(void)
+{
+	t_test_case test[] = {
+		{"hello", 5, "string 'hello'"},
+		{"", 0, "empty string"},
+		{"a", 1, "one char"},
+		{"\n", 1, "new line"},
+		{"hello\tworld", 11, "text with tab"},
+		{NULL, 0, NULL}
+	};
+	int i = 0;
+	size_t result;
+	printf("testing ft_memmove...\n");
+	while (test[i].input != NULL)
+	{
+		result = ft_memmove(test[i].input);
+		if (result == memmove(test[i].input))
+			printf("%s, " , test[i].desciption);
+		else
+		{
+			printf("fail at %s, got %zu, expeected %zu", test[i].desciption, result, test[i].expected);
+		return (1);
+		}
+		++i;
+	}
+	printf("\ntests completed\n");
+	return (0);
+}
+*\
